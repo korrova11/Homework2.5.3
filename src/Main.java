e1e3caa3941cc3dcc031a9b30c87130911fff86c
korrova11 <korrova@yandex.ru>
2023-10-05 02:30:29 +0300

    fist commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
+import static service.AccountService.checkAccount;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            checkAccount("gjgjgj", "fhjdldh", "fhjdld");
+        } catch (WrongLoginException e) {
+            System.out.println("Неправильный логин");
+
+        } catch (WrongPasswordException e) {
+            System.out.println("Неправильный пароль");
+        }
+
     }
 }
---
+++ src/service/AccountService.java
@@ -0,0 +1,30 @@
+package service;
+
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
+public class AccountService {
+    public static void checkAccount(String login, String password, String confirmPassword) {
+
+        if (login != null && password != null && confirmPassword != null) {
+            if (!login.matches("^[a-zA-Z0-9_]{1,20}$")) {
+                throw new WrongLoginException();
+            }
+            if (!(password.matches("^[a-zA-Z0-9_]{1,20}$") && (confirmPassword.matches("^[a-zA-Z0-9_]{1,20}$")) &&
+                    (password.equals(confirmPassword)))) {
+                throw new WrongPasswordException();
+            }
+
+            if (!login.matches("^[a-zA-Z0-9_]{1,20}$")) {
+                throw new WrongLoginException();
+            }
+            if (!(password.matches("^[a-zA-Z0-9_]{1,20}$") && (confirmPassword.matches("^[a-zA-Z0-9_]{1,20}$")) &&
+                    (password.equals(confirmPassword)))) {
+                throw new WrongPasswordException();
+
+            }
+
+        }
+
+    }
+}
